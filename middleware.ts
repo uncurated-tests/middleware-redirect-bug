0bf99cc0c5e77a66904675502378868efc509d14
Steven Tey <stevensteel97@gmail.com>
2023-10-10 21:51:06 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -8,10 +8,4 @@ export default function middleware(req: NextRequest) {
       "https://guides.apple.com/?ug=CglEVUIgR3VpZGUSDgjZMhDEo%2BGA%2BZKqpJUBEg4I2TIQw7y33%2B%2B6ifL%2BARIOCNkyEJC988jqgIrQjQESDgjZMhCB%2B7XSiPTwrfUBEg4I2TIQ5J25xZOynPDxARINCNkyENuVr4POz8aMcBIOCMI7EK36pfjQuerJ0gESDQjCOxDSuurnjM6T7mASDQjCOxD3vr%2F%2Fkq%2FLqUwSDQjCOxCg9cK%2BjeOhnS4%3D"
     );
   }
-
-  if (path === "/another-redirect") {
-    return NextResponse.redirect(
-      "https://guides.apple.com/?ug=CglEVUIgR3VpZGUSDgjZMhDEo+GA+ZKqpJUBEg4I2TIQw7y33++6ifL+ARIOCNkyEJC988jqgIrQjQESDgjZMhCB+7XSiPTwrfUBEg4I2TIQ5J25xZOynPDxARINCNkyENuVr4POz8aMcBIOCMI7EK36pfjQuerJ0gESDQjCOxDSuurnjM6T7mASDQjCOxD3vr//kq/LqUwSDQjCOxCg9cK+jeOhnS4="
-    );
-  }
 }
